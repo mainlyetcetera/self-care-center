4cedf98746815349800e0054072b75bfb4a02c91
mainlyetcetera <ericcampbell294@gmail.com>
2020-10-26 17:56:53 -0600

    Finish fade-in and fade-out of icon and messages and stop glitch

--- main.js
+++ main.js
@@ -35,10 +35,11 @@ var mantraRadio = document.querySelector('#mantra');
 var receiveMsg =  document.querySelector('.rcv-msg');
 var medIcon = document.querySelector('.meditation-icon');
 var msgDisplay = document.querySelector('.msg');
+var rcvMsgFunction = displayFirstMsg;
+var msg;
 var timeoutId;
-console.log(medIcon.classList);
 
-receiveMsg.addEventListener('click', displayMsg);
+receiveMsg.addEventListener('click', selectFunction);
 affRadio.addEventListener('click', determineType);
 mantraRadio.addEventListener('click', determineType);
 
@@ -57,22 +58,16 @@ function getRandomIndex(msgs) {
   return Math.floor(Math.random() * msgs.length);
 }
 
-function toggleMsgOrIcon() {
+function toggleMsgAndIcon() {
   medIcon.classList.toggle('fade-out');
   hideIcon();
-  // msgDisplay.classList.toggle('hidden');
 }
 
 function hideIcon() {
-  console.log(medIcon.classList);
   timeoutId = setTimeout(function() {
     medIcon.classList.toggle('hidden');
+    msgDisplay.classList.toggle('hidden');
   }, 3000);
-  console.log(medIcon.classList);
-}
-
-function test() {
-
 }
 
 function generateMsg() {
@@ -81,14 +76,30 @@ function generateMsg() {
   return type[randomIndex];
 }
 
-function displayMsg() {
-  var msg = generateMsg();
-  msgDisplay.innerText = msg;
-  if (!medIcon.classList.value.includes('hidden')) {
-    toggleMsgOrIcon();
-  }
+function selectFunction() {
+  rcvMsgFunction();
 }
 
-// console.log(typeof medIcon.classList.toggle('hidden'));
-// console.log(typeof generateMsg);
-console.log(medIcon.classList);
+function displayFirstMsg() {
+  msg = generateMsg();
+  msgDisplay.innerText = msg;
+  if (!medIcon.classList.value.includes('hidden')) {
+    toggleMsgAndIcon();
+  }
+
+  rcvMsgFunction = displaySecondMsg;
+}
+
+function displaySecondMsg() {
+  fadeMsgInAndOut();
+  timeoutId = setTimeout(function() {
+    msg = generateMsg();
+    msgDisplay.innerText = msg;
+    fadeMsgInAndOut();
+  }, 3000);
+}
+
+function fadeMsgInAndOut() {
+  msgDisplay.classList.toggle('fade-in');
+  msgDisplay.classList.toggle('fade-out');
+}
